dbe570956319dfd1ebd7ac624cd4f43e64633bf9
Alexandre Bossard <alexandre.bossard@tanker.io>
2019-02-19 17:10:45 +0100

    Reject messages without encrypted flag

--- app/helper_test.go
+++ app/helper_test.go
@@ -265,6 +265,9 @@ func (me *TestHelper) CreatePost(channel *model.Channel) *model.Post {
 		ChannelId: channel.Id,
 		Message:   "message_" + id,
 		CreateAt:  model.GetMillis() - 10000,
+		Props: model.StringInterface{
+			"encrypted": true,
+		},
 	}
 
 	utils.DisableDebugLogForTest()
